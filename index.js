40d7eb166ff56b98c161fd12987cde569e3fb2ef
arsya2405 <arsyanurafi@gmail.com>
2025-10-16 19:05:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,23 @@ app.use('/api', newsRouter);
 app.listen(PORT, () => {
   console.log(`✅ Server berjalan di http://localhost:${PORT}`);
 });
+
+//test nnti hapus
+/*
+import express from "express";
+import cors from "cors";
+import newsRoutes from "./src/routes/news.route.js";
+import userRoutes from "./src/routes/user.route.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Daftarkan kedua route
+app.use("/api/news", newsRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(3000, () => {
+  console.log("✅ Server running at http://localhost:3000");
+});
+*/
