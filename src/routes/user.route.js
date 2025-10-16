f3a646fbd5a6853bfe36f950d658fbb58363bdd7
arsya2405 <arsyanurafi@gmail.com>
2025-10-16 19:05:27 +0700

    Update user.route.js

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -9,3 +9,37 @@ router.put('/user/:id', updateUser);
 router.delete('/user/:id',deleteUser);
 
 module.exports = router;
+
+//test nnti hapus
+/*
+import express from "express";
+import { PrismaClient } from "../generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+// GET semua user
+router.get("/", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Tambah user
+router.post("/", async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const newUser = await prisma.user.create({
+      data: { name, email },
+    });
+    res.json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+export default router;
+*/
