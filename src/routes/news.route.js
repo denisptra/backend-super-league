c2dd3b3baa4a89ee94c1c6aa02ecdb89301b71e6
arsya2405 <arsyanurafi@gmail.com>
2025-10-17 21:53:46 +0700

    Update news.route.js

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -9,3 +9,48 @@ router.put('/news/:id', updateNews);
 router.delete('/news/:id', deleteNews);
 
 module.exports = router;
+
+//test nnti hapus
+/*
+import express from "express";
+import { PrismaClient } from "../generated/prisma/index.js";
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+// GET semua berita
+router.get("/", async (req, res) => {
+  try {
+    const news = await prisma.news.findMany();
+    res.json(news);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Tambah berita baru
+router.post("/", async (req, res) => {
+  const { title, content } = req.body;
+  try {
+    const newNews = await prisma.news.create({
+      data: { title, content },
+    });
+    res.json(newNews);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+export default router;
+*/
+//test 2 nnti hapus
+/*
+import express from "express";
+import { getAllNews, createNews } from "../controllers/newsController.js";
+const router = express.Router();
+
+router.get("/", getAllNews);
+router.post("/", createNews);
+
+export default router;
+*/
